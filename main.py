6492004f4434ca2df4452bc7f8443cb524d63532
Amaanfareedi <amaanfareedi946@gmail.com>
2026-09-17 21:13:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
-import os
 from highrise import BaseBot
 from highrise.__main__ import BotDefinition
 
 class MyBot(BaseBot):
     async def on_start(self, session_metadata) -> None:
-        print(f"Bot connected to {session_metadata.room_info.room_name}!")
-        # Bot join karte hi msg karega
-        await self.highrise.chat("Bot Online! Type !ping")
+        print("Bot Online!")
+        await self.highrise.chat("Bot is Online! !ping likho")
 
     async def on_user_join(self, user) -> None:
-        await self.highrise.chat(f"Welcome {user.username} to the room!")
+        await self.highrise.chat(f"Welcome {user.username}!")
 
     async def on_chat(self, user, message: str) -> None:
-        msg = message.lower()
-        
-        if msg == "!ping":
-            await self.highrise.chat(f"Pong! @{user.username}")
-        
-        elif msg == "!hi":
-            await self.highrise.chat(f"Hello @{user.username}!")
-            
-        elif msg == "!dance":
-            await self.highrise.send_emote("dance", user.id)
+        if message.lower() == "!ping":
+            await self.highrise.chat(f"Pong @{user.username}")
+        elif message.lower() == "!hi":
+            await self.highrise.chat(f"Hello @{user.username}")
 
-# Bot ko run karne ke liye ye sabse important hai
 if __name__ == "__main__":
     BotDefinition(MyBot()).run()
