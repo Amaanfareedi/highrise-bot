41c69b6d3bc0973a7391f2117b550018eb4c972a
Amaanfareedi <amaanfareedi946@gmail.com>
2026-09-17 20:54:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-import subprocess
 from highrise import BaseBot
 
 class MyBot(BaseBot):
@@ -16,11 +14,4 @@ class MyBot(BaseBot):
             await self.highrise.send_emote("emote-kiss")
         elif message.lower() == "!hi":
             await self.highrise.chat(f"Hello !")
-
-if __name__ == "__main__":
-    room_id = os.getenv("ROOM_ID")
-    token = os.getenv("BOT_TOKEN")
-    
-    # Ab yeh teeno arguments ek sath properly pass honge
-    subprocess.run(["highrise", "main.py", "MyBot", room_id, token])
-    
+            
