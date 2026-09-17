e449fae2983979cc411616e8c2619a5a67f59c8b
Amaanfareedi <amaanfareedi946@gmail.com>
2026-09-17 20:52:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 from highrise import BaseBot
 
@@ -17,6 +18,9 @@ class MyBot(BaseBot):
             await self.highrise.chat(f"Hello !")
 
 if __name__ == "__main__":
-    # Highrise CLI automatically environment variables (BOT_TOKEN, ROOM_ID) read kar lega
-    subprocess.run(["highrise", "main.py", "MyBot"])
-            
+    room_id = os.getenv("ROOM_ID")
+    token = os.getenv("BOT_TOKEN")
+    
+    # Ab yeh teeno arguments ek sath properly pass honge
+    subprocess.run(["highrise", "main.py", "MyBot", room_id, token])
+    
